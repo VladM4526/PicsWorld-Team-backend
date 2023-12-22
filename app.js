f52594092529755dc84372d1351b94fff6da19a4
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-12-22 23:07:04 +0100

    swagger on lokalhost 3001

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 import express from 'express';
 
+// SWAGGER
+
+import swaggerui from 'swagger-ui-express';
+
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+	definition: {
+		openapi: '5.0.0',
+		info: {
+			title: 'PicsWorldProject API',
+			version: '1.0.0',
+			description: 'Documentation for PicsWorldProject',
+		},
+		servers: [
+			{
+				url: 'http://localhost:3001',
+			},
+		],
+	},
+	apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
+
 const app = express();
+
+app.use('/api-docs', swaggerui.serve, swaggerui.setup(specs));
+
+// SWAGGER
 
 app.get('/', (request, response) => {
 	response.send(0);
